e402da6dcb9e0a295978dade9a366575055e4ff7
Adhiemas <140488222+Adhiemas@users.noreply.github.com>
2023-07-26 15:52:03 +0700

    Add files via upload

---
+++ TestGrid.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+mygrid = make_grid(5,5)
+mygrid[0][0].write('Judul')
+mygrid[1][1].write('Data 1')
+mygrid[2][2].write('Data 2')
+mygrid[3][3].write('Data 3')
+mygrid[4][4].write('Data 4')
+
+
+# In[ ]:
+
+
+
+
