33dd93221c5d87bd9c898d2c810ad77a683f1357
Adhiemas <140488222+Adhiemas@users.noreply.github.com>
2023-07-26 12:08:29 +0700

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -22,7 +22,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Uber pickups in Indonesia')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
